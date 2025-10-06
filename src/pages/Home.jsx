8f0dfaeaf8607f626c6559e51b4b472958814bfa
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-10-06 16:26:22 -0300

    style(Home, app.css): adiciona layout e estilos para a página home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
+import { artigos } from '../data/dados';
+import {Link} from 'react-router-dom';
+import React from 'react';
+
 export default function Home() {
+    const destaque = artigos.slice(0, 3);
+
     return(
-        <div>Home</div>
+        <div className='home'>
+            <h1>Bem-vindo ao Blog sobre os ODS!</h1>
+            <p>Este é um espaço dedicado a informar, educar e conscientizar sobre o ODS 16: Paz, Justiça e Instituições Eficazes.</p>
+            <p>Aqui você encontrará artigos detalhados, notícias e informações importantes que mostram como a paz, a justiça e instituições eficientes são fundamentais para um mundo mais justo e seguro para todos.</p>
+            <p>Explore nossas páginas para aprender mais:</p>
+
+            <h2>Artigos em destaque</h2>
+            <div className='artigos-destaque'>
+                <ul>
+                    {destaque.map(artigo => (
+                        <li key={artigo.id}>
+                            <Link to={`/artigo/${artigo.id}`}>{artigo.titulo}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <p> Nosso objetivo é que você compreenda a importância de cada ação e como podemos construir juntos um futuro mais pacífico e justo.</p>
+        </div>
     )
 }
